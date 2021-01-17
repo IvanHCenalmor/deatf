48509ca15e34d1ef5342748e582a7aa55b628ce5
Iván Hidalgo <ivanhcenalmor@gmail.com>
2021-01-17 18:00:24 +0100

    Files without change
    All the files of EvoFlow that do not need any change

---
+++ data.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+#import keras
+
+def load_fashion():
+
+    fashion_mnist = tf.keras.datasets.fashion_mnist.load_data()
+    #fashion_mnist = keras.datasets.fashion_mnist.load_data()
+    
+    x_train = fashion_mnist[0][0]
+    y_train = fashion_mnist[0][1]
+
+    x_test = fashion_mnist[1][0]
+    y_test = fashion_mnist[1][1]
+
+    return x_train, y_train, x_test, y_test
+
+def load_mnist():
+
+    mnist = tf.keras.datasets.mnist.load_data()
+    #mnist =  keras.datasets.mnist.load_data()
+ 
+    x_train = mnist[0][0]
+    y_train = mnist[0][1]
+
+    x_test = mnist[1][0]
+    y_test = mnist[1][1]
+
+    return x_train, y_train, x_test, y_test
---
+++ simple.py
@@ -0,0 +1,26 @@
+"""
+This is a use case of EvoFlow
+
+In this instance, we require a simple, single-DNN layer classifier for which we specify the predefined loss and fitness function.
+"""
+from data import load_fashion
+from sklearn.preprocessing import OneHotEncoder
+from Network import MLPDescriptor
+import numpy as np
+from evolution import Evolving
+from metrics import ret_evals
+
+if __name__ == "__main__":
+
+    x_train, y_train, x_test, y_test = load_fashion()
+
+    OHEnc = OneHotEncoder(categories='auto')
+
+    y_train = OHEnc.fit_transform(np.reshape(y_train, (-1, 1))).toarray()
+
+    y_test = OHEnc.fit_transform(np.reshape(y_test, (-1, 1))).toarray()
+
+    e = Evolving(loss="XEntropy", desc_list=[MLPDescriptor], x_trains=[x_train], y_trains=[y_train], x_tests=[x_test], y_tests=[y_test], evaluation="Accuracy_error", n_inputs=[[28, 28]], n_outputs=[[10]], batch_size=150, population=30, generations=10, iters=1000, n_layers=10, max_layer_size=20)
+    a = e.evolve()
+    np.save("simple_res_rand.npy", np.array(ret_evals()))
+    print(a)
