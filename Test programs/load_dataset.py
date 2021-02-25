d98855a6371f4cf114f8d366ea0a069fea0f632f
Iván Hidalgo <ivanhcenalmor@gmail.com>
2021-02-25 09:45:47 +0100

    Datasets and testing fixed

--- Test programs/load_dataset.py
+++ Test programs/load_dataset.py
@@ -12,57 +12,67 @@ def load_dataset(dataset_name):
                 'parkinsons': load_parkinsons
                 }
     
-    features, labels = datasets[dataset_name]()
+    features, labels, mode = datasets[dataset_name]()
     
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.33, random_state=42)
     
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, mode
 
 def load_mushrooms():    
-    features, labels = load_csv("./mushroom/mushroom.csv",'\\t',
-                                'class',['class'])
-    return features, labels
+    features, labels = load_csv("./mushroom/mushroom.csv",'\\t', '.',
+                                'class',[])
+    return features, labels, 'class'
 
 def load_air_quality():
-    features, labels = load_csv("./air_quality/AirQualityUCI.csv",';',
-                                'CO(GT)',['Date','Time','CO(GT)','Unnamed: 15','Unnamed: 15'])
-    return features, labels
+    features, labels = load_csv("./air_quality/AirQualityUCI.csv",';', ',',
+                                'CO(GT)',['Date','Time','Unnamed: 15','Unnamed: 16'])
+    
+    labels = list(map(float, labels))
+    
+    return features, labels, 'regr'
 
 def load_estambul_values():
-    features, labels = load_csv("./estambul_values/data_akbilgic.csv",',',
-                                'ISE',['date','ISE','Unnamed: 10','Unnamed: 11'])
-    return features, labels
+    features, labels = load_csv("./estambul_values/data_akbilgic.csv",',', '.',
+                                'ISE',['date','Unnamed: 10','Unnamed: 11'])
+    return features, labels, 'regr'
 
 def load_forest_fires():
-    features, labels = load_csv("./forest_fires/forestfires.csv",',',
-                                'area',['month', 'day', 'area'])
-    return features, labels
+    features, labels = load_csv("./forest_fires/forestfires.csv",',', '.',
+                                'area',['month', 'day'])
+    return features, labels, 'regr'
 
 def load_forest_types():
-    train_features, train_labels = load_csv("./forest_types/training.csv",',',
-                                            'class',['class'])
-    test_features, test_labels = load_csv("./forest_types/testing.csv",',',
-                                            'class',['class'])
+    train_features, train_labels = load_csv("./forest_types/training.csv",',', '.',
+                                            'class',[])
+    test_features, test_labels = load_csv("./forest_types/testing.csv",',', '.',
+                                            'class',[])
     
     labels = np.concatenate((train_labels, test_labels), axis=0)
     features = np.concatenate((train_features, test_features), axis=0)
     
-    return features, labels
+    labels = list(map(ord, map(lambda x: x.strip(), labels)))
+    
+    return features, labels, 'class'
 
 def load_parkinsons():
-    features, labels = load_csv("./parkinsons/parkinsons.csv",',',
-                                'status',['status', 'name'])
-    return features, labels
+    features, labels = load_csv("./parkinsons/parkinsons.csv", ',', ',',
+                                'status',['name'])
+    return features, labels, 'class'
 
-def load_csv(data_directory, data_sep, label_column, removed_columns):
+def load_csv(data_directory, data_sep, decimal, label_column, removed_columns):
     
-    if label_column not in removed_columns:
-        removed_columns += [label_column]
-    
-    data = pd.read_csv(data_directory, sep=data_sep)
-    
-    labels = data[label_column].values
+    data = pd.read_csv(data_directory, sep=data_sep, decimal=decimal, engine='python')    
     data = data.drop(columns=removed_columns)
+    data = data.dropna()
+
+    labels = data[label_column].values
+
+    data = data.drop(columns=label_column)
     features = np.array(data)
     
     return features, labels
+
+if __name__ == "__main__":
+    #features, labels, model = load_air_quality()
+    features, labels, model = load_forest_types()
+    #X_train, X_test, y_train, y_test, mode = load_dataset('mushrooms')
--- Test programs/test.py
+++ Test programs/test.py
@@ -3,36 +3,90 @@ sys.path.append('../')
 
 from evolution import Evolving
 from Network import MLPDescriptor
+
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
 from load_dataset import load_dataset
 
-def test(dataset_name):
+def test(dataset_name, loss_func=None, eval_func=None, batch_size=150, population=5, 
+         generations=10, iters=100, n_layers=10, max_layer_size=20):
     
-    x_train, x_test, y_train, y_test = load_dataset(dataset_name)
+    x_train, x_test, y_train, y_test, mode = load_dataset(dataset_name)
     
-    OHEnc = OneHotEncoder()
-
-    y_train = OHEnc.fit_transform(np.reshape(y_train, (-1, 1))).toarray()
-
-    y_test = OHEnc.fit_transform(np.reshape(y_test, (-1, 1))).toarray()
-
+    if not isinstance(y_train[0], float):
+        OHEnc = OneHotEncoder()
     
-    e = Evolving(loss="XEntropy", 
+        y_train = OHEnc.fit_transform(np.reshape(y_train, (-1, 1))).toarray()
+        y_test = OHEnc.fit_transform(np.reshape(y_test, (-1, 1))).toarray()
+    else:
+        y_train = np.reshape(y_train, (-1, 1))
+        y_test = np.reshape(y_test, (-1, 1))
+        
+    input_shape = x_train.shape[1:]
+    output_shape = y_train.shape[1:]
+    
+    if loss_func == None:
+        loss_func = select_loss(mode)
+    if eval_func == None:
+        eval_func = select_evaluation(mode)
+    
+    e = Evolving(loss=loss_func, 
 			 desc_list=[MLPDescriptor], 
 			 x_trains=[x_train], y_trains=[y_train], 
 			 x_tests=[x_test], y_tests=[y_test], 
-			 evaluation="Accuracy_error", 
-			 n_inputs=(22), 
-			 n_outputs=[1], 
-			 batch_size=150, 
-			 population=30, 
-			 generations=10, 
-			 iters=100, 
-			 n_layers=10, 
-			 max_layer_size=20)
+			 evaluation=eval_func, 
+			 n_inputs=[input_shape],
+			 n_outputs=[output_shape],
+			 batch_size=batch_size,
+			 population=population,
+			 generations=generations,
+			 iters=iters, 
+			 n_layers=n_layers, 
+			 max_layer_size=max_layer_size)   
+     
     a = e.evolve()
-    print(a)
+    return a
+
+def select_loss(mode):
     
-test('mushrooms')
+    if mode == "class":
+        loss = "XEntropy"
+    if mode == "regr":
+        loss = "MSE"
+        
+    return loss
+
+def select_evaluation(mode):
+    
+    if mode == "class":
+        evaluation = "Accuracy_error"
+    if mode == "regr":
+        evaluation = "MSE"
+        
+    return evaluation
+
+def test_all_datasets(loss_func=None, eval_func=None, batch_size=150, population=5, 
+                      generations=10, iters=100, n_layers=10, max_layer_size=20):
+    
+    dataset_collection = ['mushrooms', 'air_quality', 'estambul_values', 
+                          'forest_fires', 'forest_types', 'parkinsons']
+
+    for dataset in dataset_collection:
+        
+        print('\nEvaluating the {} dataset with the following configuration:'.format(dataset),
+              '\nBatch size:  {}\nPopulation of networks:  {}\nGenerations:  {}'.format(batch_size, population, generations),
+              '\nIterations in each network:  {}\nMaximum number of layers:  {}'.format(iters, n_layers),
+              '\nMaximum number of neurons in each layer: {}'.format(max_layer_size))
+        
+        try:
+            x = test(dataset, loss_func=None, eval_func=None, batch_size=batch_size, 
+                 population=population, generations=generations, iters=iters, 
+                 n_layers=n_layers, max_layer_size=max_layer_size)
+            #print(x)
+        except:
+            print('An error ocurred executing the {} dataset.'.format(dataset))        
+    
+if __name__ == "__main__":
+    #test('forest_types')
+    test_all_datasets(batch_size=200, population=2, generations = 2, iters=10)
